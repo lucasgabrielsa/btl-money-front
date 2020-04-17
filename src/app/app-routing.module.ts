94d3ada20c6c96d777200b72fadcd1ab44067411
Lucas Gabriel S. Alves <lucasgabrielsa@hotmail.com>
2020-04-16 23:23:44 -0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { PessoasCadastroComponent } from './pessoas-cadastro/pessoas-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '*', component: LancamentoCadastroComponent },
+  { path: 'lancamentos/cadastro', component: LancamentoCadastroComponent },
+  { path: 'lancamentos/pesquisa', component: LancamentosPesquisaComponent },
+  { path: 'pessoas/cadastro', component: PessoasCadastroComponent },
+  { path: 'pessoas/pesquisa', component: PessoasPesquisaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
